e52792e3ae30160e9f723e05e0253d5e9d2df540
p-akash <patel.akash@outlook.com>
2019-08-13 16:53:48 +0530

    add ingredients danamic

--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import Burger from "../../components/Burger/Burger";
 class BurgerBuilder extends Component {
-  state = {};
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  };
   render() {
     return (
       <React.Fragment>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Burger Control</div>
       </React.Fragment>
     );
