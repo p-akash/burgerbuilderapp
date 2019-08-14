1db0eb8764c54078844601ea1add418af149badb
p-akash <patel.akash@outlook.com>
2019-08-14 11:55:23 +0530

    Add Ingredients

--- src/components/Burger/BuildControls/Buildcontrols.js
+++ src/components/Burger/BuildControls/Buildcontrols.js
@@ -11,7 +11,11 @@ const buildcontrols = props => {
   return (
     <div className={classes.BuildControls}>
       {controls.map(ctrl => (
-        <BuildControl key={ctrl.label} label={ctrl.label} />
+        <BuildControl
+          key={ctrl.label}
+          label={ctrl.label}
+          added={() => props.addIngredients(ctrl.type)}
+        />
       ))}
     </div>
   );
--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/Buildcontrols";
+const INGREDIENT_PRICES = {
+  salad: 0.4,
+  bacon: 0.5,
+  cheese: 0.7,
+  meat: 1
+};
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
@@ -8,13 +14,25 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0
-    }
+    },
+    totalPrice: 4
   };
+  addIngredientsHandler = type => {
+    const updatedCount = this.state.ingredients[type] + 1;
+    const updatedIngredients = { ...this.state.ingredients };
+    updatedIngredients[type] = updatedCount;
+    const newTotalPrice = this.state.totalPrice + INGREDIENT_PRICES;
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: newTotalPrice
+    });
+  };
+
   render() {
     return (
       <React.Fragment>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls addIngredients={this.addIngredientsHandler} />
       </React.Fragment>
     );
   }
